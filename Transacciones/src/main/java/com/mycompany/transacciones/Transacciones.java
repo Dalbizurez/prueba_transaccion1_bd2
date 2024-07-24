ecc041f63eb53e17abfd86d45d59b3e5bdf8be6e
Massy100 <massielle.coti@gmail.com>
2024-07-24 12:59:14 -0600

    primer commit

---
+++ Transacciones/src/main/java/com/mycompany/transacciones/Transacciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.transacciones;
+
+/**
+ *
+ * @author Massielle
+ */
+public class Transacciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
