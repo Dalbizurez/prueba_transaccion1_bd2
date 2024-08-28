706287a534966326fc1b2747fe95321ba8f5c4fc
Massy100 <massielle.coti@gmail.com>
2024-08-27 22:22:48 -0600

    import y export

---
+++ Transacciones/database/scriptPostgres.sql
@@ -0,0 +1,20 @@
+-- Crear la base de datos transacciones
+CREATE DATABASE transacciones;
+
+-- Conectar a la base de datos transacciones
+\c transacciones;
+
+-- Crear la tabla cliente
+CREATE TABLE cliente (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    direccion VARCHAR(255)
+);
+
+-- Crear la tabla telefono
+CREATE TABLE telefono (
+    id SERIAL PRIMARY KEY,
+    number VARCHAR(20) NOT NULL,
+    cliente_id INT REFERENCES cliente(id) ON DELETE CASCADE
+);
